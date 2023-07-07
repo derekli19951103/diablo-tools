e0e7bec1f265a3cab1e58cc2bf2cfcff7504f587
nekonekokun <32277391+derekli19951103@users.noreply.github.com>
2023-07-06 23:37:28 -0400

    add background

--- app/page.tsx
+++ app/page.tsx
@@ -4,158 +4,48 @@ import {
   ReductionFormularParams,
   displayPercentage,
   getDamageReduction,
-  updated_damage_reduction_category,
 } from "@/helpers/reduction_formular";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Space, Statistic, Switch, InputNumber } from "antd";
+import { Button, Form, InputNumber, Space, Statistic, Switch } from "antd";
 import { useForm } from "antd/es/form/Form";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [form] = useForm<ReductionFormularParams>();
-  const [fortified, setFortified] = useState(false);
-  const [extraReduction, setExtraReduction] = useState(0);
-  const [damageReduction, setDamageReduction] =
-    useState<DamageReductionResult>();
-  const [reductionComparisons, setReductionComparisons] = useState<{
-    fortitied_damage_reduction: DamageReductionResult;
-    close_damage_reduction: DamageReductionResult;
-    distant_damage_reduction: DamageReductionResult;
-    overall_damage_reduction: DamageReductionResult;
-    class_damage_reduction: DamageReductionResult;
-    unique_damage_reductions?: DamageReductionResult[];
-  }>();
-  const [maxCloseReduction, setMaxCloseReduction] = useState(0);
-  const [maxDistantReduction, setMaxDistantReduction] = useState(0);
 
-  // useEffect(() => {
-  //   form.setFieldsValue({
-  //     close_damage_reduction: 0,
-  //     distant_damage_reduction: 0,
-  //     overall_damage_reduction: 0,
-  //     class_damage_reduction: 0,
-  //   });
-  // }, []);
+  const [damageReduction, setDamageReduction] = useState<DamageReductionResult>(
+    {
+      close_damage_taken: 1,
+      distant_damage_taken: 1,
+    }
+  );
 
   useEffect(() => {
     form.setFieldsValue({
-      close_damage_reduction: 67.3,
-      distant_damage_reduction: 6,
-      overall_damage_reduction: 19.2,
-      class_damage_reduction: 16,
-      fortitied_damage_reduction: 29.5,
-      is_fortified: true,
-      unique_damage_reductions: [40, 19.9],
+      close_damage_reduction: 0,
+      distant_damage_reduction: 0,
+      overall_damage_reduction: 0,
+      class_damage_reduction: 0,
+      fortitied_damage_reduction: 0,
     });
   }, []);
 
-  const updateComparison = (
-    values: ReductionFormularParams,
-    extraReduction: number
-  ) => {
-    const results = {
-      fortitied_damage_reduction: getDamageReduction({
-        ...values,
-        fortitied_damage_reduction: updated_damage_reduction_category(
-          values.fortitied_damage_reduction || 0,
-          extraReduction
-        ),
-      }),
-      close_damage_reduction: getDamageReduction({
-        ...values,
-        close_damage_reduction: updated_damage_reduction_category(
-          values.close_damage_reduction,
-          extraReduction
-        ),
-      }),
-      distant_damage_reduction: getDamageReduction({
-        ...values,
-        distant_damage_reduction: updated_damage_reduction_category(
-          values.distant_damage_reduction,
-          extraReduction
-        ),
-      }),
-      overall_damage_reduction: getDamageReduction({
-        ...values,
-        overall_damage_reduction: updated_damage_reduction_category(
-          values.overall_damage_reduction,
-          extraReduction
-        ),
-      }),
-      class_damage_reduction: getDamageReduction({
-        ...values,
-        class_damage_reduction: updated_damage_reduction_category(
-          values.class_damage_reduction,
-          extraReduction
-        ),
-      }),
-      unique_damage_reductions: values.unique_damage_reductions?.map((v, i) => {
-        let newArray = [...values.unique_damage_reductions!];
-        newArray[i] = updated_damage_reduction_category(v, extraReduction);
-        return getDamageReduction({
-          ...values,
-          unique_damage_reductions: newArray,
-        });
-      }),
-    };
-
-    setReductionComparisons(results);
-
-    setMaxCloseReduction(
-      Math.min(
-        results.fortitied_damage_reduction.close_damage_reduction,
-        results.close_damage_reduction.close_damage_reduction,
-        results.overall_damage_reduction.close_damage_reduction,
-        results.class_damage_reduction.close_damage_reduction,
-        ...(results.unique_damage_reductions?.map(
-          (u) => u.close_damage_reduction
-        ) || [])
-      )
-    );
-
-    setMaxDistantReduction(
-      Math.min(
-        results.fortitied_damage_reduction.distant_damage_reduction,
-        results.distant_damage_reduction.distant_damage_reduction,
-        results.overall_damage_reduction.distant_damage_reduction,
-        results.class_damage_reduction.distant_damage_reduction,
-        ...(results.unique_damage_reductions?.map(
-          (u) => u.distant_damage_reduction
-        ) || [])
-      )
-    );
-  };
-
-  console.log(reductionComparisons);
-
-  const getStatisticStyle = (a?: number, b?: number, isClosed?: boolean) => {
-    if (a === b) {
-      return {};
-    }
-
-    const comparison = (a || 0) - (b || 0) < 0;
-
-    return {
-      valueStyle: {
-        color: comparison ? "#cf1322" : "#3f8600",
-      },
-      prefix:
-        ((isClosed && maxCloseReduction === a) ||
-          (!isClosed && maxDistantReduction === a)) &&
-        "★",
-    };
-  };
-
   return (
-    <div>
-      <div className="grid grid-cols-2 gap-1">
-        <div className="bg-gray-300 p-5 m-5 rounded">
+    <div
+      style={{
+        backgroundImage: "url(/background.png)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="h-screen"
+    >
+      <div className="grid grid-rows-2 gap-1 h-screen">
+        <div className="bg-gray-300 p-5 m-5 rounded opacity-75">
           <Form
             layout="horizontal"
             onValuesChange={(_, values) => {
-              setFortified(values.is_fortified);
+              console.log(values);
               setDamageReduction(getDamageReduction(values));
-              updateComparison(values, extraReduction);
             }}
             form={form}
           >
@@ -247,222 +137,15 @@ export default function Home() {
           </Form>
         </div>
 
-        <div className="bg-gray-300 p-5 m-5 rounded">
+        <div className="bg-gray-300 p-5 m-5 rounded opacity-75">
           <Statistic
             title="Damage reduction from close enemies"
-            value={displayPercentage(damageReduction?.close_damage_reduction)}
+            value={displayPercentage(damageReduction?.close_damage_taken)}
           />
           <Statistic
             title="Damage reduction from distant enemies"
-            value={displayPercentage(damageReduction?.distant_damage_reduction)}
+            value={displayPercentage(damageReduction?.distant_damage_taken)}
           />
-        </div>
-
-        <div className="bg-gray-300 p-5 m-5 rounded">
-          <div className="text-black mb-2">
-            Extra damage reduction about to be applied
-          </div>
-
-          <InputNumber
-            addonAfter="%"
-            min={0}
-            max={100}
-            value={extraReduction}
-            onChange={(v) => {
-              setExtraReduction(v!);
-              updateComparison(form.getFieldsValue(), v!);
-            }}
-          />
-        </div>
-
-        <div className="bg-gray-300 p-5 m-5 rounded">
-          {fortified && (
-            <div>
-              <div className="text-black mb-2">
-                Extra Damage Reduction applied to Fortified Damage Reduction
-              </div>
-              <Statistic
-                title="Damage reduction from close enemies"
-                value={displayPercentage(
-                  reductionComparisons?.fortitied_damage_reduction
-                    .close_damage_reduction
-                )}
-                {...getStatisticStyle(
-                  reductionComparisons?.fortitied_damage_reduction
-                    .close_damage_reduction,
-                  damageReduction?.close_damage_reduction,
-                  true
-                )}
-              />
-              <Statistic
-                title="Damage reduction from distant enemies"
-                value={displayPercentage(
-                  reductionComparisons?.fortitied_damage_reduction
-                    .distant_damage_reduction
-                )}
-                {...getStatisticStyle(
-                  reductionComparisons?.fortitied_damage_reduction
-                    .distant_damage_reduction,
-                  damageReduction?.distant_damage_reduction,
-                  false
-                )}
-              />
-            </div>
-          )}
-
-          <div>
-            <div className="text-black mb-2">
-              Extra Damage Reduction applied to Close Damage Reduction
-            </div>
-            <Statistic
-              title="Damage reduction from close enemies"
-              value={displayPercentage(
-                reductionComparisons?.close_damage_reduction
-                  .close_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.close_damage_reduction
-                  .close_damage_reduction,
-                damageReduction?.close_damage_reduction,
-                true
-              )}
-            />
-            <Statistic
-              title="Damage reduction from distant enemies"
-              value={displayPercentage(
-                reductionComparisons?.close_damage_reduction
-                  .distant_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.close_damage_reduction
-                  .distant_damage_reduction,
-                damageReduction?.distant_damage_reduction,
-                false
-              )}
-            />
-          </div>
-
-          <div>
-            <div className="text-black mb-2">
-              Extra Damage Reduction applied to Distant Damage Reduction
-            </div>
-            <Statistic
-              title="Damage reduction from close enemies"
-              value={displayPercentage(
-                reductionComparisons?.distant_damage_reduction
-                  .close_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.distant_damage_reduction
-                  .close_damage_reduction,
-                damageReduction?.close_damage_reduction,
-                true
-              )}
-            />
-            <Statistic
-              title="Damage reduction from distant enemies"
-              value={displayPercentage(
-                reductionComparisons?.distant_damage_reduction
-                  .distant_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.distant_damage_reduction
-                  .distant_damage_reduction,
-                damageReduction?.distant_damage_reduction,
-                false
-              )}
-            />
-          </div>
-
-          <div>
-            <div className="text-black mb-2">
-              Extra Damage Reduction applied to Overall Damage Reduction
-            </div>
-            <Statistic
-              title="Damage reduction from close enemies"
-              value={displayPercentage(
-                reductionComparisons?.overall_damage_reduction
-                  .close_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.overall_damage_reduction
-                  .close_damage_reduction,
-                damageReduction?.close_damage_reduction,
-                true
-              )}
-            />
-            <Statistic
-              title="Damage reduction from distant enemies"
-              value={displayPercentage(
-                reductionComparisons?.overall_damage_reduction
-                  .distant_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.overall_damage_reduction
-                  .distant_damage_reduction,
-                damageReduction?.distant_damage_reduction,
-                false
-              )}
-            />
-          </div>
-
-          <div>
-            <div className="text-black mb-2">
-              Extra Damage Reduction applied to Class Damage Reduction
-            </div>
-            <Statistic
-              title="Damage reduction from close enemies"
-              value={displayPercentage(
-                reductionComparisons?.class_damage_reduction
-                  .close_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.class_damage_reduction
-                  .close_damage_reduction,
-                damageReduction?.close_damage_reduction,
-                true
-              )}
-            />
-            <Statistic
-              title="Damage reduction from distant enemies"
-              value={displayPercentage(
-                reductionComparisons?.class_damage_reduction
-                  .distant_damage_reduction
-              )}
-              {...getStatisticStyle(
-                reductionComparisons?.class_damage_reduction
-                  .distant_damage_reduction,
-                damageReduction?.distant_damage_reduction,
-                false
-              )}
-            />
-          </div>
-
-          {reductionComparisons?.unique_damage_reductions?.map((unique, i) => (
-            <div key={i}>
-              <div className="text-black mb-2">
-                Extra Damage Reduction applied to Unique Damage Reduction
-              </div>
-              <Statistic
-                title="Damage reduction from close enemies"
-                value={displayPercentage(unique?.close_damage_reduction)}
-                {...getStatisticStyle(
-                  unique.close_damage_reduction,
-                  damageReduction?.close_damage_reduction,
-                  true
-                )}
-              />
-              <Statistic
-                title="Damage reduction from distant enemies"
-                value={displayPercentage(unique?.distant_damage_reduction)}
-                {...getStatisticStyle(
-                  unique.distant_damage_reduction,
-                  damageReduction?.distant_damage_reduction,
-                  false
-                )}
-              />
-            </div>
-          ))}
         </div>
       </div>
     </div>
--- helpers/reduction_formular.ts
+++ helpers/reduction_formular.ts
@@ -1,3 +1,5 @@
+import { round } from "mathjs";
+
 export interface ReductionFormularParams {
   is_fortified: boolean;
   fortitied_damage_reduction?: number;
@@ -9,25 +11,12 @@ export interface ReductionFormularParams {
 }
 
 export interface DamageReductionResult {
-  close_damage_reduction: number;
-  distant_damage_reduction: number;
+  close_damage_taken: number;
+  distant_damage_taken: number;
 }
 
-export const updated_damage_reduction_category = (
-  original_damage_reduction: number,
-  extra_damage_reduction: number
-) => {
-  const result =
-    100.0 *
-    (1 -
-      (1 - original_damage_reduction * 0.01) *
-        (1 - 0.01 * extra_damage_reduction));
-
-  return result;
-};
-
-export const displayPercentage = (value?: number) => {
-  return value ? `${((1 - value) * 100).toFixed(3)}%` : "0%";
+export const displayPercentage = (value: number) => {
+  return `${round((1.0 - value) * 100, 3)}%`;
 };
 
 export const getDamageReduction = (
@@ -43,35 +32,29 @@ export const getDamageReduction = (
     unique_damage_reductions,
   } = params;
 
-  console.log(params);
-
-  const total_unique_damage_reduction =
+  const total_unique_damage_taken =
     unique_damage_reductions?.reduce((a, b) => a * (1.0 - b * 0.01), 1.0) ||
     1.0;
 
   let result = {
-    close_damage_reduction:
-      1.0 *
+    close_damage_taken:
       (1.0 - close_damage_reduction * 0.01) *
       (1.0 - class_damage_reduction * 0.01) *
       (1.0 - overall_damage_reduction * 0.01) *
-      total_unique_damage_reduction,
-    distant_damage_reduction:
-      1.0 *
+      total_unique_damage_taken,
+    distant_damage_taken:
       (1.0 - distant_damage_reduction * 0.01) *
       (1.0 - class_damage_reduction * 0.01) *
       (1.0 - overall_damage_reduction * 0.01) *
-      total_unique_damage_reduction,
+      total_unique_damage_taken,
   };
 
   if (is_fortified) {
-    result.close_damage_reduction *=
-      0.9 * (1 - (fortitied_damage_reduction || 0.0) * 0.01);
-    result.distant_damage_reduction *=
-      0.9 * (1 - (fortitied_damage_reduction || 0.0) * 0.01);
+    result.close_damage_taken *=
+      0.9 * (1.0 - (fortitied_damage_reduction || 0) * 0.01);
+    result.distant_damage_taken *=
+      0.9 * (1.0 - (fortitied_damage_reduction || 0) * 0.01);
   }
-
-  console.log(result);
 
   return result;
 };
