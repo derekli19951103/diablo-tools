5f9c7fb52aa8e166643bb449d78f4f71614a6d3c
nekonekokun <32277391+derekli19951103@users.noreply.github.com>
2023-07-14 19:18:45 -0400

    udpate instruction

--- app/page.tsx
+++ app/page.tsx
@@ -27,13 +27,27 @@ export default function Home() {
         >
           Diablo Damage Reduction Calculator
         </h1>
-        <div className="flex pt-10 px-10" style={{ height: "100%" }}>
+        <div className="flex pt-10 px-10">
           <div className="w-1/2 mr-5">
             <Adsense client="ca-pub-5961109963651073" slot="1134980782" />
           </div>
           <div className="w-1/2">
             <DRCalculator />
           </div>
+        </div>
+        <div className="text-center text-white my-10 text-2xl">Instruction</div>
+        <div className="text-center text-white mt-10">
+          Since not every class has fortified shield, enable the fortified if
+          you do have fortified status. For the class damage reduction, it means
+          your damage reduction from bleeding, burning, posioned or affected by
+          shadow damage and so on. Unique damage reduction, you could add this
+          section as many as you want based on those damage reduction you have,
+          etc: spells which could grant you damage reduction, or damage
+          reduction from Vulerable enemies. In other words, any damage reduction
+          not listed above could be added here. At the end, calculator will help
+          you calculate your final damage reduction against close enemy and
+          distant enemy, FYI, this calculator did not include amor system so
+          damage reduction benefit from amor has been excluded so far.{" "}
         </div>
       </div>
     </ConfigProvider>
